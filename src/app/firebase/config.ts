ade20e96a371e443585b47408d48912dc4c5340b
Liam-Dev1 <liamkeith.mariano.cics@ust.edu.ph>
2025-02-12 00:19:00 +0800

    FIX ASAP SIGNUP-FORM.TSX
    SIGNUP-FORM.TSX NOT WORKING YET

---
+++ src/app/firebase/config.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHcy0dzLUQ7y3rGzrSggSPH0tYvwOuqWY",
+  authDomain: "cics-task-management.firebaseapp.com",
+  projectId: "cics-task-management",
+  storageBucket: "cics-task-management.firebasestorage.app",
+  messagingSenderId: "8408950164",
+  appId: "1:8408950164:web:547aad1d79a1a6e0d39b81",
+  measurementId: "G-4R4Q56DN12"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
